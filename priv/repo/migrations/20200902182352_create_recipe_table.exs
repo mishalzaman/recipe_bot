7d3078980a89911e2ce46a762ce39fd04ffd38c7
Mishal Zaman <mishalzaman@gmail.com>
2020-09-03 10:34:44 -0400

    update migration

--- priv/repo/migrations/20200902182352_create_recipe_table.exs
+++ priv/repo/migrations/20200902182352_create_recipe_table.exs
@@ -5,9 +5,10 @@ defmodule RecipeBot.Repo.Migrations.CreateRecipeTable do
     create table(:recipes, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :title, :string
-      add :prep, :string
-      add :cook, :string
-      add :serving, :string
+      add :prep_minutes, :integer
+      add :cook_minutes, :integer
+      add :total_minutes, :integer
+      add :serving, :float
       add :ingredients, {:array, :string}
       add :directions, {:array, :string}
       add :nutrition_information, {:array, :string}
